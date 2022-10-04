96ab5a183196e5c6f4d4d8c9ff71de19cd9accde
Daniel Carrillo <dscv3719@gmail.com>
2022-10-03 21:28:38 -0500

    update index routes

--- server/routes/IndexRoutes.ts
+++ server/routes/IndexRoutes.ts
@@ -1,15 +1,9 @@
 import { Router, Request, Response } from "express";
-import { openDb } from "../src/db";
 
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
 	res.send("Welcome to the API of the project");
-});
-
-router.get("/ping", async (req: Request, res: Response) => {
-	const [rows] = await openDb().then((db) => db.all("SELECT 1 + 1 AS result"));
-	res.json(rows);
 });
 
 export default router;
