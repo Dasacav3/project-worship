70cadec23991771c577553113c43011390bdb001
Daniel Carrillo <dscv3719@gmail.com>
2022-10-03 21:28:51 -0500

    update index file

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,22 @@
 import express, { Express } from "express";
+import IndexRoutes from "../routes/IndexRoutes";
+import FilesRoutes from "../routes/FilesRoutes";
+import options from "../../config";
 import dotenv from "dotenv";
-import IndexRoutes from "../routes/index.routes";
-import FilesRoutes from "../routes/files.routes";
+import bodyParser from "body-parser";
+import path from "path";
 
 dotenv.config();
 
 const app: Express = express();
 
-app.use(IndexRoutes);
-app.use(FilesRoutes);
+app.use('/', IndexRoutes);
+app.use('/files', FilesRoutes);
+app.use(options.corsOptions);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use('/uploads', express.static(path.resolve('uploads')));
 
 app.listen(process.env.PORT || 3000, () =>
 	console.log(`Server running on port ${process.env.PORT || 3000}`)
