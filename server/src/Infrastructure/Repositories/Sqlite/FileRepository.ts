61c38e9eb1a6111a6492950261a59e54c822428b
Daniel Carrillo <dscv3719@gmail.com>
2022-10-03 21:29:35 -0500

    add file repository

---
+++ server/src/Infrastructure/Repositories/Sqlite/FileRepository.ts
@@ -0,0 +1,23 @@
+import IDatabaseRepository from "../../../Domain/Repositories/Database/IDatabaseRepository";
+import { openDb } from "../../../db";
+import File from "../../../Domain/Entity/File/File";
+
+export default class FileRepository implements IDatabaseRepository {
+	constructor() {}
+
+	public async save(file: File): Promise<void> {
+		const db = await openDb();
+		await db.run(
+			"INSERT INTO files (id, name, type, size, path, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?)",
+			[
+				file.getId(),
+				file.getName(),
+				file.getType(),
+				file.getSize(),
+				file.getPath(),
+				file.getCreatedAt(),
+				file.getUpdatedAt(),
+			]
+		);
+	}
+}
