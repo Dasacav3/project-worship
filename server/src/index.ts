e856b52e8eef9d1aea405da7afb96d56fcdb71ce
Daniel Carrillo <dscv3719@gmail.com>
2022-10-05 07:49:52 -0500

    use bibles routes on index

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Express } from 'express';
 import IndexRoutes from '../routes/IndexRoutes';
 import FilesRoutes from '../routes/FilesRoutes';
+import BiblesRoutes from '../routes/BiblesRoutes';
 import options from '../../config';
 import dotenv from 'dotenv';
 import path from 'path';
@@ -12,6 +13,8 @@ const app: Express = express();
 app.use('/', IndexRoutes);
 
 app.use('/files', FilesRoutes);
+
+app.use('/bibles', BiblesRoutes);
 
 app.use(options.corsOptions);
 
