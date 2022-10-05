b987351d013ccfd2cda454d38613f46d524823ea
Daniel Carrillo <dscv3719@gmail.com>
2022-10-05 13:19:41 -0500

    add route to get bible content by id

--- server/routes/BiblesRoutes.ts
+++ server/routes/BiblesRoutes.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import options from '../../config';
 import { Router } from 'express';
 import GetBiblesListGetController from '../src/Controllers/Bibles/GetBiblesListGetController';
+import GetBibleByIdGetController from '../src/Controllers/Bibles/GetBibleByIdGetController';
 
 const router = Router();
 
@@ -12,5 +13,6 @@ router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
 router.get('/', new GetBiblesListGetController().execute);
+router.get('/:id', new GetBibleByIdGetController().execute);
 
 export default router;
