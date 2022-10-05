9271d83653a137f6022fb8da104a31c171a7406d
Daniel Carrillo <dscv3719@gmail.com>
2022-10-05 17:14:22 -0500

    add find method in songs repositories

--- server/src/Domain/Repositories/Sqlite/ISongRepository.ts
+++ server/src/Domain/Repositories/Sqlite/ISongRepository.ts
@@ -2,4 +2,6 @@ import Song from '../../Entity/Song/Song';
 
 export default interface ISongRepository {
   save(song: Song): Promise<void>;
+
+  find(page: number, entries: number): Promise<object>;
 }
