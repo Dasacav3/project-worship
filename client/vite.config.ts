d833c2a13b7fea664284340c35042e8080552d78
Daniel Carrillo <dscv3719@gmail.com>
2022-10-05 20:14:31 -0500

    update vite.config.ts to add postcss config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,11 @@
+import postcss from './postcss.config.js';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  css: {
+    postcss
+  }
 })
