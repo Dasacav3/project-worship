c9b9d27d33e09d4a3a698ec79f3549c26b9db89b
Daniel Carrillo <dscv3719@gmail.com>
2022-10-06 06:39:41 -0500

    format config.ts

--- config.ts
+++ config.ts
@@ -1,14 +1,22 @@
 import cors from 'cors';
 
 const config = {
-	__dirname: __dirname,
-	corsOptions: cors({
-		allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token", "Authorization", "Access-Control-Allow-Origin"],
-		credentials: true,
-		methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
-		origin: "*",
-		preflightContinue: false,
-	})
+  __dirname: __dirname,
+  corsOptions: cors({
+    allowedHeaders: [
+      'Origin',
+      'X-Requested-With',
+      'Content-Type',
+      'Accept',
+      'X-Access-Token',
+      'Authorization',
+      'Access-Control-Allow-Origin'
+    ],
+    credentials: true,
+    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+    origin: '*',
+    preflightContinue: false
+  })
 };
 
 export default config;
