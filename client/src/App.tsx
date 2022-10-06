59cad205f2f87d4566f0961de11eafc6cfed5170
Daniel Carrillo <dscv3719@gmail.com>
2022-10-05 20:16:24 -0500

    update App.tsx with react router implementation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,69 +1,20 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Backgrounds from './pages/Backgrounds';
+import Bibles from './pages/Bibles';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Songs from './pages/Songs';
+import './assets/main.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const getFile = (e: any) => {
-    e.preventDefault();
-
-    let form = e.target;
-
-    let file = form?.querySelector('input[type="file"]');
-
-    if (file) {
-      console.log(file);
-    }
-
-    let fileData = file?.files[0];
-
-    if (fileData) {
-      console.log(fileData);
-    }
-
-    let formData = new FormData();
-    formData.append('file', fileData);
-
-    fetch('http://localhost:4000/files', {
-      method: 'POST',
-      body: formData,
-      mode: 'cors',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-      .then(res => {
-        console.log(res);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  };
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-      <form onSubmit={getFile}>
-        <input type="file" name="file" />
-        <button>Submit</button>
-      </form>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/backgrounds" element={<Backgrounds />} />
+      <Route path="/songs" element={<Songs />} />
+      <Route path="/bibles" element={<Bibles />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
