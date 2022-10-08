db521c22ba71ec0785a6505cc6417510bbec5c6c
Daniel Carrillo <dscv3719@gmail.com>
2022-10-07 19:17:54 -0500

    update button component to allow links

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -2,7 +2,7 @@ const Button = (props: Props) => {
   return (
     <>
       <button className="inline-flex justify-center rounded-md border border-transparent bg-yellow-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2">
-        {props.title}
+        {props.href ? <a href={props.href}>{props.title}</a> : props.title}
       </button>
     </>
   );
@@ -10,6 +10,7 @@ const Button = (props: Props) => {
 
 type Props = {
   title: string;
+  href?: string;
 };
 
 export default Button;
