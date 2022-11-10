d15c0dd38b756080c794e5dd9bfaee5bd82ee21c
Daniel Carrillo <dscv3719@gmail.com>
2022-11-10 16:59:26 -0500

    add property disabled to button

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,7 +1,7 @@
 const Button = (props: Props) => {
   return (
     <>
-      <button onClick={props.click} className="inline-flex justify-center rounded-md border border-transparent bg-yellow-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2">
+      <button disabled={props.disabled} onClick={props.click} className="inline-flex justify-center rounded-md border border-transparent bg-yellow-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2">
         {props.href ? <a href={props.href}>{props.title}</a> : props.title}
       </button>
     </>
@@ -11,7 +11,8 @@ const Button = (props: Props) => {
 type Props = {
   title: string;
   href?: string;
-  click?: () => void;
+  disabled?: boolean;
+  click?: () => any;
 };
 
 export default Button;
