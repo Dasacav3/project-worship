8966754f0bb6a57edb07d1366939c37ba2b28437
Daniel Carrillo <dscv3719@gmail.com>
2022-11-10 16:58:01 -0500

    update backgrounds page

--- client/src/pages/Backgrounds.tsx
+++ client/src/pages/Backgrounds.tsx
@@ -1,25 +1,31 @@
 import { useState, useEffect } from 'react';
+import { ApiUrl } from '../api/env_vars';
 import Button from '../components/Button';
 import Card from '../components/Card';
 import Header from '../components/Header';
 import Modal from '../components/Modal';
 import SideBar from '../components/Sidebar';
-import WindowParent from '../components/WindowParent';
+import Uploader from '../components/Uploader';
+import Viewer from '../context/Viewer';
 
 const Backgrounds = () => {
   const [sidebarOpen, setSideBarOpen] = useState(false);
   const [dataBackgrounds, setDataBackgrounds] = useState<any>([]);
+  const [prevPage, setPrevPage] = useState('');
+  const [nextPage, setNextPage] = useState('');
+  const [buttonPrevStatus, setButtonPrevStatus] = useState(false);
+  const [buttonNextStatus, setButtonNextStatus] = useState(false);
 
   const handleViewSidebar = () => {
     setSideBarOpen(!sidebarOpen);
   };
 
   useEffect(() => {
-    fetchData();
+    fetchData(`${ApiUrl}/files`);
   }, []);
 
-  const fetchData = async () => {
-    const response = await fetch('http://localhost:4000/files', {
+  const fetchData = async (url: string): Promise<void> => {
+    const response = await fetch(url, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json'
@@ -28,6 +34,21 @@ const Backgrounds = () => {
 
     const files = await response.json();
 
+    setPrevPage(files.pagination.prev);
+    setNextPage(files.pagination.next);
+
+    if (files.pagination.prev === null) {
+      setButtonPrevStatus(true);
+    } else {
+      setButtonPrevStatus(false);
+    }
+
+    if (files.pagination.next === null) {
+      setButtonNextStatus(true);
+    } else {
+      setButtonNextStatus(false);
+    }
+
     setDataBackgrounds(files);
   };
 
@@ -35,6 +56,23 @@ const Backgrounds = () => {
     <>
       <Header title="Fondos" />
       <SideBar isOpen={sidebarOpen} toggleSidebar={handleViewSidebar} />
+      <div>
+        <div className="flex flex-row justify-center">
+          <div className="flex flex-row">
+            <Modal
+              title="Upload Multimedia Files"
+              content={
+                <div className="flex justify-center">
+                  <Uploader />
+                </div>
+              }
+              open="Upload File"
+              save="Save"
+              close="Close"
+            />
+          </div>
+        </div>
+      </div>
       <div className="containerBackgrounds">
         <div className="backgroudStructures">
           {dataBackgrounds.data
@@ -42,19 +80,24 @@ const Backgrounds = () => {
                 <Card
                   key={index}
                   title={background.name}
-                  path={`http://localhost:4000/files/${background.id}/streaming`}
+                  path={`${ApiUrl}/files/${background.id}/streaming`}
                   order={index + 1}
                   type={background.type.split('/')[0]}
+                  click={() => Viewer(`${ApiUrl}/files/${background.id}/streaming`, '')}
                 />
               ))
             : null}
         </div>
         <div className="paginationButtons">
-        <Button
+          <Button
             title="Previous"
+            click={() => fetchData(`${ApiUrl}/files?page=${prevPage}&entries=10`)}
+            disabled={buttonPrevStatus}
           />
           <Button
             title="Next"
+            click={() => fetchData(`${ApiUrl}/files?page=${nextPage}&entries=10`)}
+            disabled={buttonNextStatus}
           />
         </div>
       </div>
