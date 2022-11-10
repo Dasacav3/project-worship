3d600c1d2e50c6b59c076de7f71d3fe0fd69e59a
Daniel Carrillo <dscv3719@gmail.com>
2022-11-10 17:00:03 -0500

    update modal css styles

--- client/src/components/Modal.tsx
+++ client/src/components/Modal.tsx
@@ -30,9 +30,7 @@ const Modal = (props: Props) => {
                   </button>
                 </div>
                 {/*body*/}
-                <div className="relative p-6 flex-auto">
-                  <p className="my-4 text-slate-500 text-lg leading-relaxed">{props.content}</p>
-                </div>
+                <div className="relative p-6 flex-auto">{props.content}</div>
                 {/*footer*/}
                 <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
                   <button
@@ -40,10 +38,10 @@ const Modal = (props: Props) => {
                     type="button"
                     onClick={() => setShowModal(false)}
                   >
-                    {props.save}
+                    {props.close}
                   </button>
                   <button
-                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    className="bg-yellow-600 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
                     onClick={() => setShowModal(false)}
                   >
