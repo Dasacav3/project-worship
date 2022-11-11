eec6de43ceda9f7f546dd96e37d3118d47b3acb1
Daniel Carrillo <daniel.carrillo@mango-soft.com>
2022-11-10 21:17:16 -0500

    update viewer component

--- client/src/components/Viewer.tsx
+++ client/src/components/Viewer.tsx
@@ -1,10 +1,39 @@
-const Viewer = (url?: string, message?: string) => {
+import { useEffect, useState } from 'react';
+import { LocalUrl } from '../api/env_vars';
+
+const Viewer = () => {
+  const [urlFile, setUrlFile] = useState('');
+  const [textContent, setTextContent] = useState('');
+  const [fileType, setFileType] = useState('video');
+
+  document.body.style.overflow = 'hidden';
+
+  useEffect(() => {
+    window.addEventListener('message', (e) => {
+      if (e.origin !== LocalUrl) {
+        return;
+      }
+
+      if (e.data.urlFile != 'undefined') {
+        setUrlFile(e.data.urlFile);
+      }
+
+      if (e.data.textContent != 'undefined') {
+        setTextContent(e.data.textContent);
+      }
+
+      if (e.data.fileType != 'undefined') {
+        setFileType(e.data.fileType);
+      }
+    });
+  }, []);
+
   return (
     <>
-      <div className='viewer'>
-        <video className='video' src={url} autoPlay loop muted />
+      <div className="viewer">
+        {fileType == 'video' ? <video className='background-effect' src={urlFile} autoPlay loop muted /> : <img className='background-effect' src={urlFile} alt="background" />}
         <div>
-          <p className='viewerContent'>{message}</p>
+          <p className="viewerContent">{textContent}</p>
         </div>
       </div>
     </>
