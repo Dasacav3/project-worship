bdd0a8079cd0b3c814e8866674a51c091f36cb42
Daniel Carrillo <dscv3719@gmail.com>
2022-11-20 15:08:54 -0500

    validation to open viewer with default file

--- client/src/components/Viewer.tsx
+++ client/src/components/Viewer.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { LocalUrl } from '../api/env_vars';
+import { ApiUrl, LocalUrl } from '../api/env_vars';
 
 const Viewer = () => {
   const [urlFile, setUrlFile] = useState('');
@@ -35,7 +35,7 @@ const Viewer = () => {
   };
 
   useEffect(() => {
-    window.addEventListener('message', (e) => {
+    window.addEventListener('message', e => {
       console.trace(e);
       if (e.origin !== LocalUrl) {
         return;
@@ -70,7 +70,16 @@ const Viewer = () => {
   return (
     <>
       <div className="viewer">
-        {fileType == 'video' ? <video className='background-effect' src={urlFile} autoPlay loop muted /> : <img className='background-effect' src={urlFile} alt="background" />}
+        {fileType == 'video' && urlFile != '' ? (
+          <video className="background-effect" src={urlFile} autoPlay loop muted />
+        ) : (
+          <video className="background-effect" src={`${ApiUrl}/files/default/streaming`} autoPlay loop muted />
+        )}
+        {fileType == 'image' && urlFile != '' ? (
+          <img className="background-effect" src={urlFile} alt="background" />
+        ) : (
+          <></>
+        )}
         <div>
           <p className="viewerContent">{textContent}</p>
         </div>
