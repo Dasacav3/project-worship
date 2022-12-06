f0e9cbaf37bb724a4e807a04f90d4e15093685cc
Daniel Carrillo <dscv3719@gmail.com>
2022-12-05 20:47:31 -0500

    update background styles

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -39,111 +39,126 @@ const Footer = ({ windowVisor }: any) => {
     <>
       <div className="w-full h-20 bg-gray-200 footer">
         <div className="grid grid-rows-2 grid-flow-col gap-0">
-          <Range
-            step={1}
-            min={-100}
-            max={100}
-            values={marginLeft}
-            onChange={values => {
-              setMarginLeft(values);
-              setMarginLeftViewer();
-            }}
-            renderTrack={({ props, children }) => (
-              <div {...props} className="w-full h-3 pr-2 my-4 bg-gray-500 rounded-md">
-                {children}
-              </div>
-            )}
-            renderThumb={({ props }) => (
-              <div
-                {...props}
-                className="w-5 h-5 transform translate-x-10 bg-indigo-500 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              />
-            )}
-          />
-          <Range
-            step={1}
-            min={-100}
-            max={100}
-            values={marginRight}
-            onChange={values => {
-              setMarginRight(values);
-              setMarginRightViewer();
-            }}
-            renderTrack={({ props, children }) => (
-              <div {...props} className="w-full h-3 pr-2 my-4 bg-gray-500 rounded-md">
-                {children}
-              </div>
-            )}
-            renderThumb={({ props }) => (
-              <div
-                {...props}
-                className="w-5 h-5 transform translate-x-10 bg-indigo-500 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              />
-            )}
-          />
-          <Range
-            step={1}
-            min={-100}
-            max={100}
-            values={marginBottom}
-            onChange={values => {
-              setMarginBottom(values);
-              setMarginBottomViewer();
-            }}
-            renderTrack={({ props, children }) => (
-              <div {...props} className="w-full h-3 pr-2 my-4 bg-gray-500 rounded-md">
-                {children}
-              </div>
-            )}
-            renderThumb={({ props }) => (
-              <div
-                {...props}
-                className="w-5 h-5 transform translate-x-10 bg-indigo-500 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              />
-            )}
-          />
-          <Range
-            step={1}
-            min={-100}
-            max={100}
-            values={marginTop}
-            onChange={values => {
-              setMarginTop(values);
-              setMarginTopViewer();
-            }}
-            renderTrack={({ props, children }) => (
-              <div {...props} className="w-full h-3 pr-2 my-4 bg-gray-500 rounded-md">
-                {children}
-              </div>
-            )}
-            renderThumb={({ props }) => (
-              <div
-                {...props}
-                className="w-5 h-5 transform translate-x-10 bg-indigo-500 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              />
-            )}
-          />
-          <Range
-            step={1}
-            min={20}
-            max={400}
-            values={fontSize}
-            onChange={values => {
-              setFontSize(values);
-              setFontSizeViewer();
-            }}
-            renderTrack={({ props, children }) => (
-              <div {...props} className="w-full h-3 pr-2 my-4 bg-gray-500 rounded-md">
-                {children}
-              </div>
-            )}
-            renderThumb={({ props }) => (
-              <div
-                {...props}
-                className="w-5 h-5 transform translate-x-10 bg-indigo-500 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              />
-            )}
-          />
+          <div>
+            <label className="range-label">Margin Left</label>
+            <Range
+              step={1}
+              min={-100}
+              max={100}
+              values={marginLeft}
+              onChange={values => {
+                setMarginLeft(values);
+                setMarginLeftViewer();
+              }}
+              renderTrack={({ props, children }) => (
+                <div {...props} className="w-full h-3 pr-2 my-4 bg-gray-500 rounded-md">
+                  {children}
+                </div>
+              )}
+              renderThumb={({ props }) => (
+                <div
+                  {...props}
+                  className="w-5 h-5 transform translate-x-10 bg-indigo-500 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                />
+              )}
+            />
+          </div>
+          <div>
+            <label className="range-label">Margin Right</label>
+            <Range
+              step={1}
+              min={-100}
+              max={100}
+              values={marginRight}
+              onChange={values => {
+                setMarginRight(values);
+                setMarginRightViewer();
+              }}
+              renderTrack={({ props, children }) => (
+                <div {...props} className="w-full h-3 pr-2 my-4 bg-gray-500 rounded-md">
+                  {children}
+                </div>
+              )}
+              renderThumb={({ props }) => (
+                <div
+                  {...props}
+                  className="w-5 h-5 transform translate-x-10 bg-indigo-500 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                />
+              )}
+            />
+          </div>
+          <div>
+            <label className="range-label">Margin Bottom</label>
+            <Range
+              step={1}
+              min={-100}
+              max={100}
+              values={marginBottom}
+              onChange={values => {
+                setMarginBottom(values);
+                setMarginBottomViewer();
+              }}
+              renderTrack={({ props, children }) => (
+                <div {...props} className="w-full h-3 pr-2 my-4 bg-gray-500 rounded-md">
+                  {children}
+                </div>
+              )}
+              renderThumb={({ props }) => (
+                <div
+                  {...props}
+                  className="w-5 h-5 transform translate-x-10 bg-indigo-500 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                />
+              )}
+            />
+          </div>
+          <div>
+            <label className="range-label">Margin Top</label>
+            <Range
+              step={1}
+              min={-100}
+              max={100}
+              values={marginTop}
+              onChange={values => {
+                setMarginTop(values);
+                setMarginTopViewer();
+              }}
+              renderTrack={({ props, children }) => (
+                <div {...props} className="w-full h-3 pr-2 my-4 bg-gray-500 rounded-md">
+                  {children}
+                </div>
+              )}
+              renderThumb={({ props }) => (
+                <div
+                  {...props}
+                  className="w-5 h-5 transform translate-x-10 bg-indigo-500 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                />
+              )}
+            />
+          </div>
+          <div>
+            <label className="range-label">Font Size</label>
+            <Range
+              step={1}
+              min={20}
+              max={400}
+              values={fontSize}
+              onChange={values => {
+                setFontSize(values);
+                setFontSizeViewer();
+              }}
+              renderTrack={({ props, children }) => (
+                <div {...props} className="w-full h-3 pr-2 my-4 bg-gray-500 rounded-md">
+                  {children}
+                </div>
+              )}
+              renderThumb={({ props }) => (
+                <div
+                  {...props}
+                  className="w-5 h-5 transform translate-x-10 bg-indigo-500 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                />
+              )}
+            />
+          </div>
           <div className="flex flex-col justify-center"></div>
           <Modal
             title="Upload Multimedia Files"
