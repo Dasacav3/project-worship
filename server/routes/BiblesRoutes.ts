a7d80f2092ef6dd9c8b67f9198114a6eae2c6b3f
Daniel Carrillo <dscv3719@gmail.com>
2022-12-25 17:17:46 -0500

    add route to get bible structure

--- server/routes/BiblesRoutes.ts
+++ server/routes/BiblesRoutes.ts
@@ -3,6 +3,7 @@ import options from '../../config';
 import { Router } from 'express';
 import GetBiblesListGetController from '../src/Controllers/Bibles/GetBiblesListGetController';
 import GetBibleByIdGetController from '../src/Controllers/Bibles/GetBibleByIdGetController';
+import GetBibleStructureGetController from '../src/Controllers/Bibles/GetBibleStructureGetController';
 
 const router = Router();
 
@@ -13,6 +14,7 @@ router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
 router.get('/', new GetBiblesListGetController().execute);
+router.get('/structure', new GetBibleStructureGetController().execute);
 router.get('/:id', new GetBibleByIdGetController().execute);
 
 export default router;
