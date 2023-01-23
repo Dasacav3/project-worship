f51fdcbea16911511b6225694d6278dfa447a461
Daniel Carrillo <dscv3719@gmail.com>
2023-01-22 19:12:54 -0500

    create script for app initialization

---
+++ execute.sh
@@ -0,0 +1,21 @@
+# Npm packages for backend
+
+npm install -g postcss-cli autoprefixer cssnano
+
+npm install
+
+# Npm packages for frontend
+
+cd client
+
+npm install
+
+# Compile tailwindcss and postcss
+
+postcss src/assets/index.css -o src/assets/main.css
+
+# Run the app
+
+cd ..
+
+npm run dev
