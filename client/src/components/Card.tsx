27400c2d723740c7230f8c02c1f091809a4b7e72
Daniel Carrillo <dscv3719@gmail.com>
2023-03-18 13:56:37 -0500

    implement button to delete file

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -1,3 +1,5 @@
+import Button from './Button';
+
 const Card = (props: Props) => {
   return (
     <div className="wrapper wrapperAnime" onClick={props.click}>
@@ -10,18 +12,23 @@ const Card = (props: Props) => {
           )}
         </div>
       </div>
-      <div className="textWrapper">
-        <h1 className="text">{`${props.order}. ${props.title}`}</h1>
+      <div className="textWrapper flex justify-center">
+        <Button
+          title={<span className="material-icons">delete_forever</span>}
+          click={event => props.delete(event, props.id)}
+        />
       </div>
     </div>
   );
 };
 
 interface Props {
+  id: string;
   title: string;
   order: number;
   path: string;
   type: string;
+  delete: (event: React.MouseEvent<HTMLButtonElement>, id: string) => void;
   click?: () => void;
 }
 
