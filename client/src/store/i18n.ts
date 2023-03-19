75363835ba75b722354cd5c8efaf62bad79abecf
Daniel Carrillo <dscv3719@gmail.com>
2023-03-18 22:05:00 -0500

    add first translation implementation

---
+++ client/src/store/i18n.ts
@@ -0,0 +1,24 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import en from '../assets/lang/en.json';
+import es from '../assets/lang/es.json';
+
+const resources = {
+  en: {
+    translation: en
+  },
+  es: {
+    translation: es
+  }
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: 'en',
+  fallbackLng: 'en',
+  interpolation: {
+    escapeValue: false
+  }
+});
+
+export default i18n;
