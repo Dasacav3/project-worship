e5647a40378398918338d5b5f60c7c0b1977f5ce
Daniel Carrillo <dscv3719@gmail.com>
2023-03-22 08:39:09 -0500

    update scripts

---
+++ execute.cmd
@@ -0,0 +1,13 @@
+:: Npm packages for backend
+npm install -g postcss-cli postcss
+npm install
+
+:: Npm packages for frontend
+cd client
+npm install
+
+:: Compile tailwindcss and postcss
+npx postcss src/assets/index.css -o src/assets/main.css
+
+:: Run the app
+cd ..
