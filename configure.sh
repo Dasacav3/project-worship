75c838da7e5b23a179990a4cbcbd7d37d74130c6
Daniel Carrillo <dscv3719@gmail.com>
2023-08-13 21:47:57 -0500

    add scripts for configure and sinstall depedencies

---
+++ configure.sh
@@ -0,0 +1,16 @@
+# Install jq
+sudo apt update && sudo apt install jq -y
+
+# Npm packages for backend
+npm install -g postcss-cli postcss
+npm install
+
+# Npm packages for frontend
+cd client
+npm install
+
+# Compile tailwindcss and postcss
+postcss src/assets/index.css -o src/assets/main.css
+
+# Go to root folder
+cd ..
