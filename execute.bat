e4a11deeca1c2f2efabaf4ada61d87a2a261d7c0
Daniel Carrillo <dscv3719@gmail.com>
2023-08-13 21:47:23 -0500

    remove execute scripts

--- execute.bat
+++
@@ -1,2 +0,0 @@
-:: Npm packages for backend && Npm packages for frontend && Compile tailwindcss and postcss && Go root folder
-npm install -g postcss-cli postcss && npm install && cd client && npm install && npx postcss src/assets/index.css -o src/assets/main.css && cd ..
