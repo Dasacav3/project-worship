ea2258d0e71a3c365fac79e4e6a81bf2a89d8524
Daniel Carrillo <dscv3719@gmail.com>
2023-09-29 10:56:08 -0500

    update configure scripts and use pnpm

--- configure.ps1
+++ configure.ps1
@@ -1,2 +1,9 @@
 # Npm packages for backend && Npm packages for frontend && Compile tailwindcss and postcss && Go root folder
-npm install -g postcss-cli postcss && npm install && cd client && npm install && npx postcss src/assets/index.css -o src/assets/main.css && Set-Location ..
+npm install -g npm@latest pnpm concurrently nodemon `
+    ts-node typescript vite tailwindcss postcss `
+    autoprefixer postcss-cli postcss && `
+pnpm install && `
+Set-Location client && `
+pnpm install && `
+npx postcss src/assets/index.css -o src/assets/main.css && `
+Set-Location ..
--- configure.sh
+++ configure.sh
@@ -1,13 +1,21 @@
 # Install jq
-sudo apt update && sudo apt install jq -y
+apt update && apt install jq -y
+
+# Update npm
+npm install -g npm@latest 
+
+# Install pnpm
+npm install -g pnpm
+
+# Npm packages globally
+pnpm install concurrently nodemon ts-node typescript vite tailwindcss postcss autoprefixer postcss-cli postcss
 
 # Npm packages for backend
-npm install -g postcss-cli postcss
-npm install
+pnpm install
 
 # Npm packages for frontend
 cd client
-npm install
+pnpm install
 
 # Compile tailwindcss and postcss
 postcss src/assets/index.css -o src/assets/main.css
